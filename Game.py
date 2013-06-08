2ee954334f3939c3ad185ef2fbbb7f858e0d9609
Max Leander <max.y.leander@gmail.com>
2013-06-08 19:41:33 +0200

    Game.py now works

--- Game.py
+++ Game.py
@@ -1,16 +1,40 @@
+import pygame, sys
+from pygame.locals import *
+
 class Game:
+	windowSurface = None
+	fpsClock = pygame.time.Clock()
 
 	def run(self):
+		self.init()
 		# Game Loop
 		while(True):
-			update()
-			draw()
+			self.checkInput()
+			self.update()
+			self.draw()
+			self.fpsClock.tick(60)
+
+	def checkInput(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
 
 	def update(self):
 		pass
 
 	def draw(self):
-		pass
+		# draw the white background onto the surface
+		self.windowSurface.fill((255,255,255))
+		# draw a green polygon onto the surface
+		pygame.draw.polygon(self.windowSurface, (0,255,0), ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+		# draw the window onto the screen
+		pygame.display.update()
+
+	def init(self):
+		pygame.init()
+		# set up the window
+		self.windowSurface = pygame.display.set_mode((1024, 768), 0, 32)
 
 if  __name__ =='__main__':
 	game = Game()
