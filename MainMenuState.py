d4e56240262d64a6b2a137e4728fc08d305ce5c1
Christofer <christofer.heimonen@lunicore.se>
2013-06-09 02:54:05 +0200

    Blinking "Press enter"!

--- MainMenuState.py
+++ MainMenuState.py
@@ -3,11 +3,13 @@ from pygame.locals import *
 
 from GameState import GameState
 from SoundHandler import SoundHandler
+from Test import TestState
 
 class MainMenuState(GameState):
 
 	_title = None
 	_enter = None
+	_surface = None
 
 	def __init__(self, player):
 		pass
@@ -18,20 +20,26 @@ class MainMenuState(GameState):
 		self._enter = pygame.image.load('enter.png')
 		pygame.time.Clock() 
 
+
 	def update(self, keyDown):
-		pass
+		if K_RETURN in keyDown:
+			GameHandler.push(TestState(self._surface))
 
 	def draw(self, windowSurface):
+		self._surface = windowSurface
 		# draw the black background onto the surface
 		windowSurface.fill((0,0,0))
 		# draw the title image
 		scale = math.sin(pygame.time.get_ticks() / float(1000)) / 2 + 1
+		enterScale = math.sin(pygame.time.get_ticks() / float(300)) / 2 + 1
+		print scale
 		newSurface = pygame.transform.scale(self._title,(int(self._title.get_width() * scale), \
 		 int(self._title.get_height() * scale)))
 		windowSurface.blit(newSurface, ((windowSurface.get_width() - newSurface.get_width()) / 2, 20))
 		# Draw press enter
-		windowSurface.blit(self._enter, ((windowSurface.get_width() - self._enter.get_width()) / 2, \
-		 windowSurface.get_height() - (windowSurface.get_height() - self._enter.get_height()) / 3))
+		if enterScale < 1:
+			windowSurface.blit(self._enter, ((windowSurface.get_width() - self._enter.get_width()) / 2, \
+		 	 windowSurface.get_height() - (windowSurface.get_height() - self._enter.get_height()) / 3))
 		# draw the window onto the screen
 		pygame.display.update()
 
